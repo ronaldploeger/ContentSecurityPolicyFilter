23ce76fe927788089864a597c5d3534052e2d323
ploe09 <ploe09@W8LPLOE09.asysoffice.de>
2014-05-14 14:29:45 +0200

    javadoc

--- src/main/java/de/saville/csp/ContentSecurityPolicyFilter.java
+++ src/main/java/de/saville/csp/ContentSecurityPolicyFilter.java
@@ -19,9 +19,11 @@ import org.slf4j.LoggerFactory;
  * 
  * Also see: http://content-security-policy.com/ & http://www.w3.org/TR/CSP/#directives
  * 
- * Here is an example full configuration of the ContentSecurityPolicyFilter. 
  * Normally you will only need a limited number or none of the init parameters. If no init parameter is defined the Header will look like this:
- * Content-Security-Policy = default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; connect-src 'self'
+ * 
+ *     Content-Security-Policy = default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; connect-src 'self'
+ *     
+ * Here is an example full configuration of the ContentSecurityPolicyFilter. 
  *  
  *     <filter>
  *            <filter-name>ContentSecurityPolicyFilter</filter-name>
